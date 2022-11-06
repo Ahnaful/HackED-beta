3d9d4c0faa4ce1478c226ca8158f0df712809d55
Uzair Tariq <uzair0213@icloud.com>
2022-11-06 07:38:56 -0700

    somewhere

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import time
+import _thread
+import utime
 
+from machine import Pin
 import LightController as lc
 from getDetect import getDetect
+import prexcp as p
 
 RED_NS = 21
 YLW_NS = 20
@@ -17,22 +20,79 @@ lc.AllOff(RED_EW, YLW_EW, GRN_EW)
 lc.TurnRed(RED_NS, YLW_NS, GRN_NS)
 lc.TurnRed(RED_EW, YLW_EW, GRN_EW)
 
-while True:  # main loop
-    north_detect = getDetect(15, 28)
-    south_detect = getDetect(15, 27)
-    east_detect  = getDetect(15, 26)
+waiting_cars = {"NS": 0, "EW": 0}
+car_detected_NS = False
+car_detected_EW = False
+
+# interrupts
+def car_detected_NS(Pin):
+    global car_detected_NS
+    car_detected_NS = True
     
-    print(north_detect, south_detect, east_detect)
+def car_detected_EW(Pin):
+    global car_detected_EW
+    car_detected_EW = True
     
-    if north_detect or south_detect:
-        time.sleep(1)
-        lc.TurnGreen(RED_NS, YLW_NS, GRN_NS)
-        time.sleep(5)
-        lc.TurnRed(RED_NS, YLW_NS, GRN_NS)
     
-    if east_detect:
-        time.sleep(1)
-        lc.TurnGreen(RED_EW, YLW_EW, GRN_EW)
-        time.sleep(5)
-        lc.TurnRed(RED_EW, YLW_EW, GRN_EW)
+NS_digital = Pin(0, Pin.OUT)
+EW_digital = Pin(1, Pin.OUT)
+
+NS_digital.value(0)
+EW_digital.value(0)
+
+NS_interrupt = Pin(6, Pin.IN)
+EW_interrupt = Pin(7, Pin.IN)
+
+NS_interrupt.irq(trigger=Pin.IRQ_RISING, handler=car_detected_NS)
+EW_interrupt.irq(trigger=Pin.IRQ_RISING, handler=car_detected_EW)
+
+
+def count_cars():
+    while True:
+        global NS_digital
+        global EW_digital
+        global waiting_cars
+        
+        north_detect = getDetect(15, 28)
+        south_detect = getDetect(15, 27)
+        east_detect  = getDetect(15, 26)
+        
+        # print(north_detect, south_detect, east_detect)
+        
+        if north_detect or south_detect:
+            NS_digital.value(1)
+        
+        if east_detect:
+            EW_digital.value(1)
+            
+_thread.start_new_thread(count_cars, ())
     
+try:
+    while True:  # main loop
+        if car_detected_NS:
+            print("NS")
+            NS_digital.value(0)
+            car_detected_NS = False
+        if car_detected_EW:
+            print("EW")
+            EW_digital.value(0)
+            car_detected_EW = False
+        
+        if waiting_cars["NS"]:
+            utime.sleep(1)
+            lc.TurnGreen(RED_NS, YLW_NS, GRN_NS)
+            utime.sleep(3 * waiting_cars["NS"])
+            waiting_cars["NS"] = 0
+            lc.TurnRed(RED_NS, YLW_NS, GRN_NS)
+        
+        if waiting_cars["EW"]:
+            utime.sleep(1)
+            lc.TurnGreen(RED_EW, YLW_EW, GRN_EW)
+            utime.sleep(3 * waiting_cars["EW"])
+            waiting_cars["EW"] = 0
+            lc.TurnRed(RED_EW, YLW_EW, GRN_EW)
+        
+        utime.sleep(0.1)
+except Exception as e:
+    Pin(25, Pin.OUT).value(1)
+    p.print_exc(e)
