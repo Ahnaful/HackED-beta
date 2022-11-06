5287726bea6a88a23bcd9feb6a320080dd7b269d
Uzair Tariq <uzair0213@icloud.com>
2022-11-06 06:18:34 -0700

    basic car detection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 
 import LightController as lc
+from getDetect import getDetect
 
 RED_NS = 21
 YLW_NS = 20
@@ -17,13 +18,21 @@ lc.TurnRed(RED_NS, YLW_NS, GRN_NS)
 lc.TurnRed(RED_EW, YLW_EW, GRN_EW)
 
 while True:  # main loop
-    time.sleep(1)
-    lc.TurnGreen(RED_NS, YLW_NS, GRN_NS)
-    time.sleep(3)
-    lc.TurnRed(RED_NS, YLW_NS, GRN_NS)
+    north_detect = getDetect(15, 28)
+    south_detect = getDetect(15, 27)
+    east_detect  = getDetect(15, 26)
     
-    time.sleep(1)
-    lc.TurnGreen(RED_EW, YLW_EW, GRN_EW)
-    time.sleep(3)
-    lc.TurnRed(RED_EW, YLW_EW, GRN_EW)
+    print(north_detect, south_detect, east_detect)
+    
+    if north_detect or south_detect:
+        time.sleep(1)
+        lc.TurnGreen(RED_NS, YLW_NS, GRN_NS)
+        time.sleep(5)
+        lc.TurnRed(RED_NS, YLW_NS, GRN_NS)
+    
+    if east_detect:
+        time.sleep(1)
+        lc.TurnGreen(RED_EW, YLW_EW, GRN_EW)
+        time.sleep(5)
+        lc.TurnRed(RED_EW, YLW_EW, GRN_EW)
     
