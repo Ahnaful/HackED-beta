cc92d33b65be599e5bcd213df8a33492ed325d64
Uzair Tariq <uzair0213@icloud.com>
2022-11-06 00:51:35 -0600

    updated

--- getDetect.py
+++ getDetect.py
@@ -3,22 +3,26 @@ from machine import Pin, ADC, PWM
 import time
 
 
-def getDetect(p,a):
+def getDetect(pwm_pin, adc_pin):
 
-    pwm = PWM(Pin(p))
-    adc = ADC(a)
+    pwm = PWM(Pin(pwm_pin))
+    adc = ADC(adc_pin)
     
 
     pwm.duty_u16(adc.read_u16())
     time.sleep(0.5)
         
     if pwm.duty_u16() < 600:
-        return False
+        return False, pwm.duty_u16()
     else:
-        return True
+        return True, pwm.duty_u16()
 
 
 if __name__ == "__main__":
     while True:
-        print(getDetect(15,28))
-        time.sleep(0.5)
+        print("L: ", getDetect(15, 28))
+        print("M: ", getDetect(15, 27))
+        print("R: ", getDetect(15, 26))
+        print()
+        
+        time.sleep(2)
